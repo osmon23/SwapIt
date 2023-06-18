2c21d1168390f6d19f06e8b70d4223da5447a35b
Marat <126090537+mcilee1@users.noreply.github.com>
2023-06-18 14:28:49 +0600

    creating a chat

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,1 +1,37 @@
-a
+from rest_framework import serializers
+from .models import Category, Product, ProductImage
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(default=None)
+
+    class Meta:
+        model = Category
+        fields = (
+            'name',
+            'parent',
+        )
+        read_only_fields = (
+            'id',
+        )
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = ProductImage
+        fields = (
+            'id',
+            'product',
+            'image',
+        )
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    category = CategorySerializer(read_only=True)
+    image = ProductImageSerializer
+
+    class Meta:
+        model = Product
+        fields = '__all__'
